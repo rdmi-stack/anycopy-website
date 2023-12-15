1ded75bba1440c80d18bbcaa0bad638aaee236ef
Ranjit kumar <ranjit9119@gmail.com>
2023-12-15 06:37:01 +0530

    d

--- components/web/Hero.js
+++ components/web/Hero.js
@@ -10,25 +10,26 @@ const Modal = () => {
 
   return (
     <>
-    <>
+   <>
   {/* Hero */}
   <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
     {/* Grid */}
     <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
       <div>
         <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">
-          Get 10x More Reply With <span className="text-blue-600">Elixcent AI</span>
+          Create Copy At Scale With <span className="text-blue-600">Generative AI</span>
         </h1>
         <p className="mt-3 text-lg text-gray-800 dark:text-gray-400">
-        10x Your Reply Rates: AI-Powered Personalization for Targeted Email Outreach, at Scale. Boost Replies, Secure More Meetings, Drive Sales
+        Transform content creation with Generative AI. Effortlessly produce high-quality, engaging copy at scale, empowering your business to excel in the digital landscape with speed and precision.
         </p>
         {/* Buttons */}
         <div className="mt-7 grid gap-3 w-full sm:inline-flex">
           <a
             className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            href="https://app.elixcent.com/SignUp"
+            href="#"
           >
-Start Your 7 Days Free Trial            <svg
+            Get started For Free
+            <svg
               className="flex-shrink-0 w-4 h-4"
               xmlns="http://www.w3.org/2000/svg"
               width={24}
@@ -45,22 +46,85 @@ Start Your 7 Days Free Trial            <svg
           </a>
           <a
             className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            href="/demo"
+            href="#"
           >
-Book A Demo          </a>
+            Book A Demo
+          </a>
         </div>
         {/* End Buttons */}
         {/* Review */}
         <div className="mt-6 lg:mt-10 grid grid-cols-2 gap-x-5">
           {/* Review */}
           <div className="py-5">
-          <div className="flex space-x-1">
-    {/* Replace SVGs with image tags */}
-    <img src="/images/g2.png" alt="Review Image" className="h-4 w-4" style={{ width: '51px', height: '51px' }} />
-    {/* Add more image tags if needed */}
-  </div>
+            <div className="flex space-x-1">
+              <svg
+                className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                width={51}
+                height={51}
+                viewBox="0 0 51 51"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27.0352 1.6307L33.9181 16.3633C34.2173 16.6768 34.5166 16.9903 34.8158 16.9903L50.0779 19.1845C50.9757 19.1845 51.275 20.4383 50.6764 21.0652L39.604 32.3498C39.3047 32.6632 39.3047 32.9767 39.3047 33.2901L41.998 49.2766C42.2973 50.217 41.1002 50.8439 40.5017 50.5304L26.4367 43.3208C26.1375 43.3208 25.8382 43.3208 25.539 43.3208L11.7732 50.8439C10.8754 51.1573 9.97763 50.5304 10.2769 49.59L12.9702 33.6036C12.9702 33.2901 12.9702 32.9767 12.671 32.6632L1.29923 21.0652C0.700724 20.4383 0.999979 19.4979 1.89775 19.4979L17.1598 17.3037C17.459 17.3037 17.7583 16.9903 18.0575 16.6768L24.9404 1.6307C25.539 0.69032 26.736 0.69032 27.0352 1.6307Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <svg
+                className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                width={51}
+                height={51}
+                viewBox="0 0 51 51"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27.0352 1.6307L33.9181 16.3633C34.2173 16.6768 34.5166 16.9903 34.8158 16.9903L50.0779 19.1845C50.9757 19.1845 51.275 20.4383 50.6764 21.0652L39.604 32.3498C39.3047 32.6632 39.3047 32.9767 39.3047 33.2901L41.998 49.2766C42.2973 50.217 41.1002 50.8439 40.5017 50.5304L26.4367 43.3208C26.1375 43.3208 25.8382 43.3208 25.539 43.3208L11.7732 50.8439C10.8754 51.1573 9.97763 50.5304 10.2769 49.59L12.9702 33.6036C12.9702 33.2901 12.9702 32.9767 12.671 32.6632L1.29923 21.0652C0.700724 20.4383 0.999979 19.4979 1.89775 19.4979L17.1598 17.3037C17.459 17.3037 17.7583 16.9903 18.0575 16.6768L24.9404 1.6307C25.539 0.69032 26.736 0.69032 27.0352 1.6307Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <svg
+                className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                width={51}
+                height={51}
+                viewBox="0 0 51 51"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27.0352 1.6307L33.9181 16.3633C34.2173 16.6768 34.5166 16.9903 34.8158 16.9903L50.0779 19.1845C50.9757 19.1845 51.275 20.4383 50.6764 21.0652L39.604 32.3498C39.3047 32.6632 39.3047 32.9767 39.3047 33.2901L41.998 49.2766C42.2973 50.217 41.1002 50.8439 40.5017 50.5304L26.4367 43.3208C26.1375 43.3208 25.8382 43.3208 25.539 43.3208L11.7732 50.8439C10.8754 51.1573 9.97763 50.5304 10.2769 49.59L12.9702 33.6036C12.9702 33.2901 12.9702 32.9767 12.671 32.6632L1.29923 21.0652C0.700724 20.4383 0.999979 19.4979 1.89775 19.4979L17.1598 17.3037C17.459 17.3037 17.7583 16.9903 18.0575 16.6768L24.9404 1.6307C25.539 0.69032 26.736 0.69032 27.0352 1.6307Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <svg
+                className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                width={51}
+                height={51}
+                viewBox="0 0 51 51"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27.0352 1.6307L33.9181 16.3633C34.2173 16.6768 34.5166 16.9903 34.8158 16.9903L50.0779 19.1845C50.9757 19.1845 51.275 20.4383 50.6764 21.0652L39.604 32.3498C39.3047 32.6632 39.3047 32.9767 39.3047 33.2901L41.998 49.2766C42.2973 50.217 41.1002 50.8439 40.5017 50.5304L26.4367 43.3208C26.1375 43.3208 25.8382 43.3208 25.539 43.3208L11.7732 50.8439C10.8754 51.1573 9.97763 50.5304 10.2769 49.59L12.9702 33.6036C12.9702 33.2901 12.9702 32.9767 12.671 32.6632L1.29923 21.0652C0.700724 20.4383 0.999979 19.4979 1.89775 19.4979L17.1598 17.3037C17.459 17.3037 17.7583 16.9903 18.0575 16.6768L24.9404 1.6307C25.539 0.69032 26.736 0.69032 27.0352 1.6307Z"
+                  fill="currentColor"
+                />
+              </svg>
+              <svg
+                className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                width={51}
+                height={51}
+                viewBox="0 0 51 51"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27.0352 1.6307L33.9181 16.3633C34.2173 16.6768 34.5166 16.9903 34.8158 16.9903L50.0779 19.1845C50.9757 19.1845 51.275 20.4383 50.6764 21.0652L39.604 32.3498C39.3047 32.6632 39.3047 32.9767 39.3047 33.2901L41.998 49.2766C42.2973 50.217 41.1002 50.8439 40.5017 50.5304L26.4367 43.3208C26.1375 43.3208 25.8382 43.3208 25.539 43.3208L11.7732 50.8439C10.8754 51.1573 9.97763 50.5304 10.2769 49.59L12.9702 33.6036C12.9702 33.2901 12.9702 32.9767 12.671 32.6632L1.29923 21.0652C0.700724 20.4383 0.999979 19.4979 1.89775 19.4979L17.1598 17.3037C17.459 17.3037 17.7583 16.9903 18.0575 16.6768L24.9404 1.6307C25.539 0.69032 26.736 0.69032 27.0352 1.6307Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
             <p className="mt-3 text-sm text-gray-800 dark:text-gray-200">
-              <span className="font-bold">4.6</span> /5
+              <span className="font-bold">4.6</span> /5 - from 12k reviews
             </p>
             <div className="mt-5">
               {/* Star */}
@@ -175,7 +239,7 @@ Book A Demo          </a>
               </svg>
             </div>
             <p className="mt-3 text-sm text-gray-800 dark:text-gray-200">
-              <span className="font-bold">4.8</span> /5
+              <span className="font-bold">4.8</span> /5 - from 5k reviews
             </p>
             <div className="mt-5">
               {/* Star */}
