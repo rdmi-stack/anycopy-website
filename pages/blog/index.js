aaa92b39207ccbadbb2dbc3b1310bcdf371bc888
Ranjit9119 <info@rdmi.in>
2024-03-27 03:39:27 +0530

    blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -28,30 +28,7 @@ Anycopy BLOG          </h1>
            
           </p>
         </div>
-        {/* Buttons */}
-        <div className="text-center">
-          <a
-            className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800"
-            href="#"
-          >
-            Get started
-            <svg
-              className="flex-shrink-0 w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </a>
-        </div>
-        {/* End Buttons */}
+        
       </div>
     </div>
   </div>
