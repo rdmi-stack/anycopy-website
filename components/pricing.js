cb196b66888c2b6d175dbac80c6d93b5bcdc0508
elixcent <76124485+rdmi-stack@users.noreply.github.com>
2024-04-04 22:29:01 +0000

    fix: Update signup link to use HTTPS

--- components/pricing.js
+++ components/pricing.js
@@ -248,7 +248,7 @@ const Pricing = () => {
           </ul>
           <a
             className="mt-5 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            href="/signup"
+            href="https://app.anycopy.co/signup"
           >
             Start Your Free Trial
           </a>
